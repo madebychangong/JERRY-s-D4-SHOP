2b17c79c900a2b2dc7d869a1db4899342c2040e0
madebychangong <ppylgr@gmail.com>
2025-07-23 21:30:20 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '100px' }}>
+      <h1>💜 JERRY's D4 SHOP 💜</h1>
+      <p>이 페이지는 이미지 자동 변환 시스템의 엔트리 포인트입니다.</p>
+      <p>API 경로: <code>/api/render</code> 로 POST 요청 시 이미지가 생성됩니다.</p>
+    </div>
+  );
+}
